441fb6543de632f90169c0d89b0342b203901814
IliaD <idorokhin@gmail.com>
2021-10-15 22:09:13 +0300

    #HW4. fix .sh scripts v7

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd ~
-#cd /home/yc-user || echo "System abort, no found a folder /home/yc-user" && exit
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -3,6 +3,6 @@
 cd ~
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates mongodb-org
+sudo apt update && sudo apt install -y apt-transport-https mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod

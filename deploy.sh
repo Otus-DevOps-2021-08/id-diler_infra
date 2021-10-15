2f223e67b8743cf630456628bf1b617dda51f0eb
IliaD <idorokhin@gmail.com>
2021-10-15 21:25:28 +0300

    #HW4. fix .sh scripts v1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd /home/yc-user || echo "System abort, no found a folder /home/yc-user" && exit
+cd ~
+#cd /home/yc-user || echo "System abort, no found a folder /home/yc-user" && exit
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d

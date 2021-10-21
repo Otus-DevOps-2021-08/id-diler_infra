b6881272b4270d01bca337e4d74af9e8542a6cd2
IliaD <idorokhin@gmail.com>
2021-10-22 00:20:55 +0300

    HW6 fix test #4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.35"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source  = "yandex-cloud/yandex"
+#      version = "~> 0.35"
+#    }
+#  }
+#}
 
 provider "yandex" {
   service_account_key_file = var.yandex_service_account_key_file

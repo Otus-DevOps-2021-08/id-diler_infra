4956abbcd42b34f3ce252b076efa9c54128a8cbc
IliaD <idorokhin@gmail.com>
2021-11-12 03:51:07 +0300

    HW7, fix

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,8 +1,10 @@
 module "app" {
-  public_key_path = var.public_key_path
-  app_disk_image  = var.app_disk_image
-  subnet_id       = var.yandex_subnet_id
-  source          = "../modules/app"
+  public_key_path  = var.public_key_path
+  private_key_path = var.private_key_path
+  app_disk_image   = var.app_disk_image
+  subnet_id        = var.yandex_subnet_id
+  source           = "../modules/app"
+  database_url     = module.db.external_ip_address_db
 }
 module "db" {
   public_key_path = var.public_key_path
